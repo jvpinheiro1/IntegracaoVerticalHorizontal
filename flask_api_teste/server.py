3c27f1a5537c0610c5e50dcd6d850a50c75444d5
JVitor2002 <jvitor.pinheiro.garcia@gmail.com>
2025-02-07 19:27:15 -0300

    oi

--- flask_api_teste/server.py
+++ flask_api_teste/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 import random
+import mysql.connector
 
 
 #cria aplicação flask
@@ -9,13 +10,39 @@ app=Flask(__name__)
 #Habilita cors para permitir a requisição do javascript no servidor flask
 CORS(app)
 
+def create_connection():
+    return mysql.connector.connect (
+        host="localhost",
+        user="root",
+        password="123456",
+        database="dados"
+
+    )
+
 def get_sensor_data():
-    return{
+    data ={
         #gerar valores aleatorios
         "temperatura":round(random.uniform(20,80),2),
         "umidade":round(random.uniform(30,90),2),
         "pressao":round(random.uniform(900,1100),2)
     }
+
+    insert_sensor_data(data)
+
+    return data
+#funcao ára inserir dados no banco 
+def insert_sensor_data(data):
+    try:
+        conn = create_connection()
+        cursor = conn.cursor()
+        query = "INSERT INTO sensores(umidade,pressao,temperatura) VALUES (%s,%s,%s)"
+        cursor.execute(query, data['umidade'],data['pressao'],data['temperatura'])
+        conn.commit()
+    except mysql.connetor.Error as err:
+        print(f"Error: (err)")
+    finally:
+        cursor.close()
+        conn.close()
 
 #define a rota da API que responderá com os dados dos sensores em JSON
 @app.route('/sensores',methods=['GET'])#A rota '/sensores' responde  apenas a requisicao GET
